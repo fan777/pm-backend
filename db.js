f63787c756b5b53ee3c6e631343cd6244811c6df
fan777 <fan777@gmail.com>
2021-12-01 15:10:36 -0800

    update db.js

--- db.js
+++ db.js
@@ -13,8 +13,8 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    // connectionString: getDatabaseUri(),
-    ...connParams,
+    connectionString: getDatabaseUri(),
+    // ...connParams,
     ssl: {
       rejectUnauthorized: false
     }
