913e8dd76e0cd71e7227ac713835dd8bca81e388
fan777 <fan777@gmail.com>
2021-12-01 14:22:11 -0800

    update seed

--- pm-seed.sql
+++ pm-seed.sql
@@ -10,6 +10,8 @@ INSERT INTO portfolios (name, cash, notes, username)
 VALUES ('active',1787.95,'','cfan'),
        ('stable',10.62,'','cfan'),
        ('test',0,'test notes','test');
+       ('test2',200.12,'more notes','test');
+       ('test3',123.45,'more notes2','test');
 
 INSERT INTO holdings (symbol, shares_owned, cost_basis, target_percentage, goal, portfolio_id)
 VALUES ('ALLY', 20, 988.70, 0.1, 'HOLD', 1),
@@ -24,7 +26,9 @@ VALUES ('ALLY', 20, 988.70, 0.1, 'HOLD', 1),
        ('VXUS', 206.0241, 12117.98, 0.2, 'HOLD', 2),
        ('QQQ', 10, 1000, 0.50, 'HOLD', 3),
        ('AAPL', 0, 0, 0.25, 'BUY', 3),
-       ('TSLA', 0, 0, 0.25, 'BUY', 3);
+       ('TSLA', 1, 0, 0.25, 'BUY', 3);
+       ('M', 5, 0, 0.25, 'BUY', 4);
+       ('VISA', 15, 0, 0.25, 'BUY', 3);
 
 INSERT INTO watchlist (username, symbol)
 VALUES ('cfan','AMD'),
